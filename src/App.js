fa1ead32964aa46debd28a24e8bdff9fc5c6a702
Luis Mariano Bibo <lmbibbo@gmail.com>
2022-08-09 20:39:04 -0300

    StartUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class myComponent extends React.Component {
+  componentPlayers() {
+    const apiUrl = 'http://localhost:8080/api/v1/players';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+  }
 }
-
-export default App;
+export default myComponent;
