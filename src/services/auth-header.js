6385b0ee1ee31f99636c610e01f93dabddb8c364
Luis Mariano Bibbo <lmbibbo@gmail.com>
2022-08-17 08:16:32 -0300

    Primero con IDEA

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,8 +2,10 @@ export default function authHeader() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   if (user && user.accessToken) {
-    return { Authorization: 'Bearer ' + user.accessToken };
-    // return { "x-auth-token": user.accessToken };
+    // return { Authorization: 'Bearer ' + user.accessToken };
+    console.log("Luisma");
+    console.log(user.accessToken);
+    return { "x-auth-token": user.accessToken };
   } else {
     return {};
   }
